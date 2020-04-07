469caaea48fbe31a929ad2717bc2c8b7a2654d7f
LSJ <952373511@qq.com>
2020-04-07 17:22:19 +0800

    页面跳转

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,66 +1,26 @@
 // pages/about/about.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  // 1.从home传递数据
   onLoad: function (options) {
-
+    console.log(options);//options包含home页面传过来的数据
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
+  // 2.从about传递数据给home---修改home的数据
   onUnload: function () {
-
+    // 返回时传递数据给home页面
+    //  getCurrentPages():全局方法，获取所有在栈中的页面
+    const pages = getCurrentPages();
+    // console.log(pages);
+    // 1.获取home页面对象
+    const home = pages[0];
+    home.setData({
+      name: '哈哈哈'
+    })
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  /* 3.用API返回上一页 */
+  handleNavigateBack(){
+    wx.navigateBack({
+      // delta: 2
+    })
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
